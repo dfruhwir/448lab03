f431477acbe606453adfbd653295e3ee1dcf3711
Dillon Fruhwirth <d487f080@ku.edu>
2016-02-17 20:06:11 -0600

    Modify alert text

--- pValidator.js
+++ pValidator.js
@@ -4,14 +4,18 @@ function Validate()
 	var mp2 = document.getElementById("p2").value;
 	if((mp1 != mp2) && (mp1.length<8||mp2.length<8))
 	{
-		alert("Passwords don't match.Password length is less than 8 characters.");
+		alert("Passwords don't match.\nPassword length is less than 8 characters.");
 	}
 	else if(mp1 != mp2)
 	{
-		alert(mp1);
+		alert("Passwords don't match");
 	}
 	else if(mp1.length<8)
 	{
 		alert("Password length is less than 8 characters.");
+	}
+	else
+	{
+		alert("Passwords validated!!");
 	}
 }
