fc65e614fd141ce379fb727ef054e33418f4dd4a
Dillon Fruhwirth <d487f080@ku.edu>
2016-02-18 19:16:40 -0600

    Add alerts

--- cssChange.js
+++ cssChange.js
@@ -10,6 +10,38 @@ function change()
 	var back_green = document.getElementById("ba_g").value;
 	var back_blue = document.getElementById("ba_b").value;
 	
-	dummy.style.border = "thick solid rgb("+bor_red+","+bor_green+","+bor_blue+")";
-	dummy.style.backgroundColor = "rgb("+back_red+","+back_green+","+back_blue+")";
+	if(isNaN(bor_red)==true||bor_red%1!=0||bor_red<0||bor_red>255)
+	{
+		alert("Enter a whole number from 0 to 255 for color values.");
+	}
+	else if(isNaN(bor_green)==true||bor_green%1!=0||bor_green<0||bor_green>255)
+	{
+		alert("Enter a whole number from 0 to 255 for color values.");
+	}
+	else if(isNaN(bor_blue)==true||bor_blue%1!=0||bor_blue<0||bor_blue>255)
+	{
+		alert("Enter a whole number from 0 to 255 for color values.");
+	}
+	else if(isNaN(back_red)==true||back_red%1!=0||back_red<0||back_red>255)
+	{
+		alert("Enter a whole number from 0 to 255 for color values.");
+	}
+	else if(isNaN(back_green)==true||back_green%1!=0||back_green<0||back_green>255)
+	{
+		alert("Enter a whole number from 0 to 255 for color values.");
+	}
+	else if(isNaN(back_blue)==true||back_blue%1!=0||back_blue<0||back_blue>255)
+	{
+		alert("Enter a whole number from 0 to 255 for color values.");
+	}
+	else if(bor_width != "thick" && bor_width != "thin" && bor_width != "medium")
+	{
+		alert("Enter thin, medium, or thick for border width.");
+	}
+	else
+	{
+		
+		dummy.style.border = bor_width+" solid rgb("+bor_red+","+bor_green+","+bor_blue+")";
+		dummy.style.backgroundColor = "rgb("+back_red+","+back_green+","+back_blue+")";
+	}
 }
